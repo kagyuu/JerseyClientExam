472762c9f451a4b5df8c9f22e88544fe4aec60f4
HONDOH Atsushi <kagyuu@hondou.homedns.org>
2016-03-23 21:42:16 +0900

    Fix Comments

--- RestClient/src/main/java/com/mycompany/restclient/Main5.java
+++ RestClient/src/main/java/com/mycompany/restclient/Main5.java
@@ -1,12 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.restclient;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
--- RestClientCommon/src/main/java/com/mycompany/restclientcommon/EmployeeBean.java
+++ RestClientCommon/src/main/java/com/mycompany/restclientcommon/EmployeeBean.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.restclientcommon;
 
 import javax.xml.bind.annotation.XmlRootElement;
@@ -10,10 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-/**
- *
- * @author atsushi
- */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
